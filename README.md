6e33715ba8fae2028f8964eb274fc7dd00fc896b
david <david@aukes.com>
2025-10-07 00:24:08 +0200

    Added struct

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fluid-jdbc
 Lets you map classes to tables using annotations and allow you to generate generic DAO's and allow for querying all within java
+
+Use:
+
+```kotlin
+repositories { mavenCentral() }
+dependencies { implementation("io.github.david-auk:fluid-jdbc:0.1.0") }
+```
---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+// build.gradle.kts (module: fluid-jdbc)
+plugins {
+    kotlin("jvm") version "2.0.0"
+    `java-library`
+    `maven-publish`
+    signing
+    id("org.jetbrains.dokka") version "1.9.20"
+}
+
+group = "io.github.david-auk"
+version = "0.1.0"
+
+java {
+    toolchain { languageVersion.set(JavaLanguageVersion.of(21)) }
+    withSourcesJar()
+    withJavadocJar() // plain Javadoc jar (Dokka can replace content)
+}
+
+tasks.javadoc { enabled = false }     // optional if you only want Dokka HTML
+
+dependencies {
+    api(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
+}
+tasks.test { useJUnitPlatform() }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            pom {
+                name.set("fluid-jdbc")
+                description.set("Map classes to tables via annotations, generate generic DAOs, and query in pure Java/Kotlin.")
+                url.set("https://github.com/david-auk/fluid-jdbc")
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("david-auk")
+                        name.set("David Aukes")
+                        url.set("https://github.com/david-auk")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/david-auk/fluid-jdbc")
+                    connection.set("scm:git:https://github.com/david-auk/fluid-jdbc.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/david-auk/fluid-jdbc.git")
+                }
+            }
+        }
+    }
+}
+
+// Sign all publications using in-memory PGP (good for CI)
+signing {
+    val signingKey = System.getenv("SIGNING_KEY")      // ASCII-armored private key
+    val signingPass = System.getenv("SIGNING_PASSWORD")
+    useInMemoryPgpKeys(signingKey, signingPass)
+    sign(publishing.publications)
+}
